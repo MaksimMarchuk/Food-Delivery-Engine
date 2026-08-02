a8c68c282e52b53a002c0ee8233bd6b69051d4e7
Maksim_Marchuk <makstop280409@gmail.com>
2026-08-02 12:32:20 +0300

    Refactor product data structure and enhance menu display functionality

--- add.js
+++ add.js
@@ -1,19 +1,85 @@
-// МОДУЛЬ 1. Управління каталогом (Catalog Service)
-
-// Формування та збереження структури страв/товарів із атрибутами: ID, назва, категорія, ціна, доступність, рейтинг.
-
 var product = [
-    { id: "001", name: "Лимонад класичний", category: "Напої", price: "65", accessibility: "Немає", rating: "4.5"},
-    { id: "002", name: "Еспресо", category: "Напої", price: "45", accessibility: "В наявності", rating: " 4.8"},
-    { id: "003", name: "Піца Маргарита", category: "Основні страви", price: "180", accessibility: "В наявності", rating: "4.9"},
-    { id: "004", name: "Бургер з яловичиною", category: "Основні страви", price: "220", accessibility: "В наявності", rating: "4.7"},
-    { id: "005", name: "Паста Карбонара", category: "Основні страви", price: "195", accessibility: "Немає", rating: " 4.6"},
-    { id: "006", name: "Салат Цезар", category: "Закуски", price: "160", accessibility: "В наявності", rating: "4.8"},
-    { id: "007", name: "Картопля фрі", category: "Закуски", price: "75", accessibility: "В наявності", rating: "4.3"}
-]
+  {
+    id: "001",
+    name: "Лимонад класичний",
+    category: "Напої",
+    price: "65",
+    accessibility: "Немає",
+    rating: "4.5",
+  },
+  {
+    id: "002",
+    name: "Еспресо",
+    category: "Напої",
+    price: "45",
+    accessibility: "В наявності",
+    rating: " 4.8",
+  },
+  {
+    id: "003",
+    name: "Піца Маргарита",
+    category: "Основні страви",
+    price: "180",
+    accessibility: "В наявності",
+    rating: "4.9",
+  },
+  {
+    id: "004",
+    name: "Бургер з яловичиною",
+    category: "Основні страви",
+    price: "220",
+    accessibility: "В наявності",
+    rating: "4.7",
+  },
+  {
+    id: "005",
+    name: "Паста Карбонара",
+    category: "Основні страви",
+    price: "195",
+    accessibility: "Немає",
+    rating: " 4.6",
+  },
+  {
+    id: "006",
+    name: "Салат Цезар",
+    category: "Закуски",
+    price: "160",
+    accessibility: "В наявності",
+    rating: "4.8",
+  },
+  {
+    id: "007",
+    name: "Картопля фрі",
+    category: "Закуски",
+    price: "75",
+    accessibility: "В наявності",
+    rating: "4.3",
+  },
+];
 
-// Реалізація чистих функцій фільтрації меню за категорією, діапазоном ціни та статусом наявності.
+while (true) {
+  var mainPromt = prompt(
+    "1 - Переглянути меню \n 2 - Пошук страви за назвою \n 3 - Додати страву в кошик \n 4 - Переглянути кошик та чек \n 5 - Застосувати промокд \n 6 - Оформити замовлення \n 0 - Вийти",
+  );
 
-// Пошук товарів за частковим збігом у назві (без урахування регістру).
+  if (mainPromt === "1") {
+    const menu = product.map(({ name, price, rating }) => ({
+      name,
+      price,
+      rating,
+    }));
 
-// Сортування каталогу за ціною та рейтингом.
+    const formattedText = menu
+      .sort((a, b) => parseFloat(b.rating) - parseFloat(a.rating))
+      .map(
+        (item) => `${item.name} — ${item.price} грн (Оцінка: ${item.rating})`,
+      )
+      .join("\n");
+
+    alert(formattedText);
+  }
+
+  if (mainPromt === "0") {
+    break;
+  }
+}
